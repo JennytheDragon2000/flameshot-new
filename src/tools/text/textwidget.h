483c78b25b2324446206aae1ebb491920dd45f23
jenny <www.srinath.25.com@gmail.com>
2024-12-25 19:19:50 +0530

    initial commit

---
+++ src/tools/text/textwidget.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2017-2019 Alejandro Sirgo Rica & Contributors
+
+#pragma once
+
+#include <QTextEdit>
+
+class TextWidget : public QTextEdit
+{
+    Q_OBJECT
+public:
+    explicit TextWidget(QWidget* parent = nullptr);
+
+    void adjustSize();
+    void setFont(const QFont& f);
+    void setTextColorAndBackground(const QColor& c, const QColor& bgColor);
+
+protected:
+    void showEvent(QShowEvent* e);
+    void resizeEvent(QResizeEvent* e);
+
+signals:
+    void textUpdated(const QString& s);
+
+public slots:
+    void setTextColor(const QColor& c);
+    void setAlignment(Qt::AlignmentFlag alignment);
+
+private slots:
+    void emitTextUpdated();
+
+private:
+    QSize m_baseSize;
+    QSize m_minSize;
+};
